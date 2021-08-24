7c5c21736e597433243dbee107ff012b53573409
Alex Parker <alex.n.parker82@gmail.com>
2021-08-24 15:32:20 -0400

    creating classes and starting html template

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer');
-const jest = require('jest');
+const generateHTML = require('./src/generateHTML');
+const fs = require('fs');
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const userPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name
+        }
+    ])
+}
+
+
+const init = () => {
+
+    writeFileAsync('./dist/index.html', generateHTML.generateHTML())
+    .then(() => console.log('Successfully wrote to index.html!'))
+    .catch((err) => console.error(err));
+};
+
+init();
