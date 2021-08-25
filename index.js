83bb02d33e649aacb5de20f9aaffee465be7a1ac
Alex Parker <alex.n.parker82@gmail.com>
2021-08-25 06:08:14 -0400

    inquirer logic and test files

--- index.js
+++ index.js
@@ -32,23 +32,20 @@ const userPrompt = () => {
             message: `What is your team manager's office number?`,
         }
     ])
-    .then(function(answers) {
-        const manager = new Manager(answers.manager_id, answers.manager_name, answers.manager_email, answers.office_number);
-        console.log(manager);
-        teamArray.push(manager);
-        console.log(teamArray);
-        addMember();
-    })
-    .catch(function(err) {
-        console.error(err)
-    })
-    
+        .then(function (answers) {
+            let manager = new Manager(answers.manager_id, answers.manager_name, answers.manager_email, answers.office_number);
+            teamArray.push(manager);
+            addMember();
+        })
+        .catch(function (err) {
+            console.error(err)
+        })
+
 };
-    
+
 
 const addMember = () => {
     return inquirer.prompt([
-    
         {
             type: 'list',
             name: 'new_member',
@@ -57,87 +54,99 @@ const addMember = () => {
                 "Engineer",
                 "Intern",
                 "Done adding."
-            ],
-        },
+            ]
+        }
+    ])
+        .then((answers) => {
+            switch (answers.new_member) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+
+                case "Intern":
+                    addIntern();
+                    break;
+
+                case "Done adding.":
+                    generateHTML();
+                    break;
+            };
+        })
+        .catch((err) => console.error(err));
+};
+
+
+
+const addEngineer = () => {
+    return inquirer.prompt([
+
         {
             type: 'input',
             name: 'engineer_name',
             message: `What is your engineer's name?`,
-            when: function (answers) {
-                return answers.new_member === "Engineer";
-            }
         },
         {
             type: 'input',
             name: 'engineer_id',
             message: `What is your engineer's id number?`,
-            when: function (answers) {
-                return answers.new_member === "Engineer";
-            }
         },
         {
             type: 'input',
             name: 'engineer_email',
             message: `What is your engineer's email?`,
-            when: function (answers) {
-                return answers.new_member === "Engineer";
-            }
         },
         {
             type: 'input',
             name: 'engineer_github',
             message: `What is your engineer's GitHub profile name?`,
-            when: function (answers) {
-                return answers.new_member === "Engineer";
-            }
         },
+    ])
+        .then((answers) => {
+            let newEngineer = new Engineer(answers.engineer_id, answers.engineer_name, answers.engineer_email, answers.engineer_github);
+            teamArray.push(newEngineer);
+            addMember();
+        })
+        .catch((err) => console.error(err));
+};
+
+const addIntern = () => {
+    return inquirer.prompt([
+
         {
             type: 'input',
             name: 'intern_name',
             message: `What is your intern's name?`,
-            when: function (answers) {
-                return answers.new_member === "Intern";
-            }
+            
         },
         {
             type: 'input',
             name: 'intern_id',
             message: `What is your intern's ID number?`,
-            when: function (answers) {
-                return answers.new_member === "Intern";
-            }
+            
         },
         {
             type: 'input',
             name: 'intern_email',
             message: `What is your intern's email?`,
-            when: function (answers) {
-                return answers.new_member === "Intern";
-            }
+            
         },
         {
             type: 'input',
             name: 'intern_school',
             message: `What school did your intern attend?`,
-            when: function (answers) {
-                return answers.new_member === "Intern";
-            }
+            
         },
-    ]).then((answers) => {
-        if (answers.new_member = 'y') {
+    ])
+        .then((answers) => {
+            let newIntern = new Intern(answers.intern_id, answers.intern_name, answers.intern_email, answers.intern_school);
+            teamArray.push(newIntern);
+            console.log(teamArray);
             addMember();
-        }
-        else {
-            return;
-        }
-    })
-    .then((answers) => {
-        console.log(answers);
-        teamArray.push(answers);
-        console.log(teamArray);
-    })
-    .catch((err) => console.error(err));
+
+        })
+        .catch((err) => console.error(err));
 };
+
 
 
 
